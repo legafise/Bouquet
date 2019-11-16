385d054a86a42efc756b7e03fd24ddd5344bb8d6
legafise <lashkevich1974@gmail.com>
2019-11-16 20:15:59 +0300

    Delete unnecessary imports and shortened code

--- src/com/lashkevich/flowers/util/randomizer/AccessoryRandomizer.java
+++ src/com/lashkevich/flowers/util/randomizer/AccessoryRandomizer.java
@@ -1,7 +1,6 @@
 package com.lashkevich.flowers.util.randomizer;
 
 import com.lashkevich.flowers.entity.Accessory;
-import com.lashkevich.flowers.util.rounding.DoubleRounding;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,14 +8,13 @@ import java.util.List;
 import static com.lashkevich.flowers.entity.AccessoryName.getRandomAccessoryName;
 import static com.lashkevich.flowers.entity.Color.getRandomColor;
 import static com.lashkevich.flowers.util.generator.IdGenerator.generateAccessoryId;
+import static com.lashkevich.flowers.util.rounding.DoubleRounding.round;
 
 public class AccessoryRandomizer {
     public static List<Accessory> generateRandomAccessory() {
         List<Accessory> accessories = new ArrayList<>();
         for (int i = 0; i < ((int) (1 + Math.random() * 3)); i++) {
-            double randomPrice = DoubleRounding.round(0.1 + Math.random() * 0.4, 1);
-
-            accessories.add(new Accessory(generateAccessoryId(), getRandomAccessoryName(), getRandomColor(), randomPrice));
+            accessories.add(new Accessory(generateAccessoryId(), getRandomAccessoryName(), getRandomColor(), round(0.1 + Math.random() * 0.4, 1)));
         }
 
         return accessories;
